b9ea906b7b66511cf9d725e2bc1b973051c75dc1
Matheus Ferreira <fmatheusaugusto@gmail.com>
2021-12-27 14:14:11 -0300

    confirmar em 'v2'

--- includes/lista_livros.php
+++ includes/lista_livros.php
@@ -1,14 +1,16 @@
-﻿<tr>
-    <td>
-        <?php echo $titulo; ?>
-    </td>
-    <td>
-        <?php echo $autores; ?>
-    </td>
-    <td>
-        <?php echo $localizacao; ?>
-    </td>
-    <td>
-        <?php echo $codigo; ?>
-    </td>
-</tr>
+﻿<tbody>
+    <tr>
+        <td>
+            <?php echo $titulo; ?>
+        </td>
+        <td>
+            <?php echo $autores; ?>
+        </td>
+        <td>
+            <?php echo $localizacao; ?>
+        </td>
+        <td>
+            <?php echo $codigo; ?>
+        </td>
+    </tr>
+</tbody>
